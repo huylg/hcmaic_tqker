88736706885cf3a6a917b8cf6b1301000c9d826a
vinhdc10998 <chivinh10998@gmail.com>
2020-08-13 15:45:57 +0700

    dataset

--- editFile.py
+++ editFile.py
@@ -1,19 +1,21 @@
 import os
 from opts import *
 def rename(path):
+    i=400
     for image in os.listdir(path):
         if 'jpeg' in image:
             os.system("mv {} {}".format(os.path.join(path,image),os.path.join(path,"image_{}.jpeg".format(i))))
+            os.system("mv {} {}".format(os.path.join(path,image.replace('jpeg','txt')),os.path.join(path,"image_{}.txt".format(i))))
             i+=1
 
 def trainTxt(path):
-    trainFile = open('/home/vinh/Python/hcmaic_tqker/label/train.txt','w+')
+    trainFile = open('/home/vinh/Python/hcmaic_tqker/label/valid.txt','w+')
     u=1
     for streets in os.listdir(path):
-        for image in os.listdir(os.path.join(path,streets)):
-            if 'jpeg' in image:
-                trainFile.write("images/{streets}/{image}\n".format(streets=streets,image=image))
+        if 'jpeg' in streets:
+                trainFile.write("dataset/images/{image}\n".format(image=streets))
                 u+=1
+            
 
 if __name__ == "__main__":
     opt = parse_opts()
